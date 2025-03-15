55b426de7d4a4dca04f93847a069b9a3a2a4c340
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-15 12:11:37 +0200

    Edit: remove error import

--- src/main/java/lv/venta/service/impl/CRUDProfessorServiceImpl.java
+++ src/main/java/lv/venta/service/impl/CRUDProfessorServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import lv.venta.model.Product;
 import lv.venta.model.Professor;
 import lv.venta.model.enums.Degree;
 import lv.venta.repo.IProfessorRepo;
