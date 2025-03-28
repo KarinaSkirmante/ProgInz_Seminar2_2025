08a003e8268e700bbdcf82800d9e1df807bca575
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-28 17:46:00 +0200

    Add: created controler class with getControllerGetAllGradesForStudent function + html

--- src/main/java/lv/venta/controller/FilterController.java
+++ src/main/java/lv/venta/controller/FilterController.java
@@ -1,10 +1,15 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.model.Grade;
 import lv.venta.service.IFilterService;
 
 @Controller
@@ -16,8 +21,20 @@ public class FilterController {
 	
 	//TODO
 	//izveidot Getmapping 
-	//@GetMapping("/grades/student/{id}")
-	//funkcijas deklarācija, ar PathVariable anotāciju, lai nolasītu id
+	@GetMapping("/grades/student/{id}")
+	public String getControllerGetAllGradesForStudent(@PathVariable (name = "id") int id, Model model)
+	{
+		try
+		{
+			ArrayList<Grade> filteredGrades = filtService.selectGradesByStudentId(id);
+			model.addAttribute("package", filteredGrades);
+			return "show-grades-page";//parādīs show-grades-page.html lapu ar izfiltrētām atzīmēm
+		}catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error-page";//parādīt show-error-page.html lapu, kura būs kļudas ziņojums
+		}
+	}
+
 	//izsaukt atbilstošo servisa funkciju -> selectGradesByStudentId
 	//uztaisīt try un catch bloku
 	//catch bloka caur moedl.addatribute padot izņemuma ziņu
