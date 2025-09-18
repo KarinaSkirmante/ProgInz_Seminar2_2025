b155f36257b506a0faa71c830a632a76c84173bc
karina.krinkele@vea.lv <karina.krinkele@vea.lv@DAT-332356L>
2025-09-18 15:36:28 +0300

    Add: prpfessor crud controller - retrieveAll, retrieveOne, create

---
+++ src/main/java/lv/venta/controller/CRUDProfessorController.java
@@ -0,0 +1,84 @@
+package lv.venta.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import jakarta.validation.Valid;
+import lv.venta.model.Professor;
+
+import lv.venta.service.ICRUDProfessorService;
+
+@Controller
+@RequestMapping("/crud/professor")
+public class CRUDProfessorController {
+	
+	@Autowired
+	private ICRUDProfessorService profService;
+
+	@GetMapping("/all")//localhost:8080/crud/professor/all
+	public String getAllProfessorController(Model model) {
+		try
+		{
+			ArrayList<Professor> allProfessors = profService.retrieveAll();
+			model.addAttribute("professors", allProfessors);
+			return "show-all-professors-page";//visi profesoru dati tiks ievietoti show-all-professors-pga.html
+		}
+		catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error-page";
+		}
+		
+	}
+	
+	@GetMapping("/all/{id}")//localhost:8080/crud/professor/all/1
+	public String getOneProfessorController(Model model, @PathVariable(name = "id") int id ) {
+		try
+		{
+			Professor oneProfessor = profService.retreiveById(id);
+			model.addAttribute("package", oneProfessor);
+			return "show-one-professor-page";//parādīs show-one-professor-page.html
+		}
+		catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error-page";
+		}
+		
+	}
+	
+	@GetMapping("/create")//localhost:8080/crud/professor/create
+	public String getCreateNewProfessor(Model model) {
+		Professor emptyProfessor = new Professor();
+		model.addAttribute("professor", emptyProfessor);
+		return "create-new-professor";//atvērsies create-new-professor.html ar neaizpildītu profesoru
+	}
+	
+	@PostMapping("/create")
+	public String postCreateNewProfessor(@Valid Professor professor, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			return "create-new-professor";
+		}
+		else
+		{
+			try
+			{
+				profService.create(professor.getName(), professor.getSurname(), professor.getDegree());
+				return "redirect:/crud/professor/all";
+			}
+			catch (Exception e) {
+				model.addAttribute("package", e.getMessage());
+				return "show-error-page";
+			}
+		}
+	}
+	
+	
+}
